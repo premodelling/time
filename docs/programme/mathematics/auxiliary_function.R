a3fcd7388fed21b81ed4d058f5c3f19338172b9a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-02-22 12:30:45 +0000

    Add docs/programme/mathematics/auxiliary_function.R

---
+++ docs/programme/mathematics/auxiliary_function.R
@@ -0,0 +1,55 @@
+library(geoR)
+library(PrevMap)
+vari.time <- function(time,...) {
+  variog(coords=cbind(time,1),...)
+}
+
+vari.env <- function(variogram.t,time,...) {
+  variog.mc.env(coords=cbind(time,1),
+                obj.variog = variogram.t,...) 
+}
+
+
+fit.matern <- function(form,...,data,time) {
+  data$t_aux <- 1
+  out <- linear.model.MLE(formula=form,...,data=data,
+                   coords=as.formula(paste("~",time,"+ t_aux")))
+  out$call$formula <- form
+  out
+}
+
+
+time.predict <- function(fitted.model,predictors=NULL,time.pred,scale.pred=NULL,...) {
+  if(scale.pred=="exponential") {
+    scale.pred <- "odds"
+  } else {
+    scale.pred <- "logit"
+  }
+  time.pred <- cbind(time.pred,1)
+  fit0.5$call$formula
+  out <- spatial.pred.linear.MLE(object=fitted.model,
+                          predictors = predictors,
+                          grid.pred = time.pred,
+                          scale.predictions = scale.pred,...,
+                          messages=FALSE)
+  if(scale.pred=="odds") {
+    out$predictions <- out$odds$predictions
+    out$quantiles <- out$odds$quantiles
+  } else {
+    out$predictions <- out$logit$predictions
+    out$quantiles <- out$logit$quantiles
+  }
+  out$time.pred <- time.pred[,1]
+  out
+}
+
+
+create.lag.var <- function(x,lag) {
+  tlag <- mal$t-lag
+  ind.lag <- sapply(tlag,function(x) {
+    out <- which((mal$t-x)==0)
+    if(length(out)==0) out <- NA
+    return(out)
+  })
+  x[ind.lag]
+}
